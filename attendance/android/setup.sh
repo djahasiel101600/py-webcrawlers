d0cafcc811996b4ed6e566e78305f9b1b774bbd4
djahasiel16 <djahasiel@gmail.com>
2025-11-18 16:15:13 +0800

    updae

--- attendance/android/setup.sh
+++ attendance/android/setup.sh
@@ -6,7 +6,7 @@ pip install selenium beautifulsoup4
 # Download geckodriver for Android
 # You'll need to download the appropriate ARM64 version
 wget https://github.com/mozilla/geckodriver/releases/download/v0.36.0/geckodriver-v0.36.0-linux-aarch64.tar.gz
-tar -xzf geckodriver-v0.34.0-linux-aarch64.tar.gz
+tar -xzf geckodriver-v0.36.0-linux-aarch64.tar.gz
 chmod +x geckodriver
 mv geckodriver /data/data/com.termux/files/usr/bin/
 
