6c3ac1edd975bdfd0a403c5403548d93060dbf52
djahasiel16 <djahasiel@gmail.com>
2026-01-30 16:30:24 +0800

    Fix circular import

--- attendance/android/methods.py
+++ attendance/android/methods.py
@@ -6,7 +6,6 @@ from rich.text import Text
 import requests
 from soundNotifier import SoundNotifier
 from attendanceRecord import AttendanceRecord
-from methods import send_telegram_message
 
 
 console = Console()
